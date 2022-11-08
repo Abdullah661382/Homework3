886fc27e3d7ece4de162cf8b1bbbbbb570c15b1d
Abdullah661382 <AbdullahAdel711@gmail.com>
2022-11-08 00:05:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def Formula1(a, b, c):
+    d = (b**2) - (4*a*c)
+    return (- b + d**0.5)/(2*a)
+
+
+def Formula2(a, b, c):
+    e = (b**2) - (4*a*c)
+    return (- b + e**0.5)/(2*a)
+
+
+print(Formula1(1, 2, -3))
+print(Formula2(4, 5, -6))
